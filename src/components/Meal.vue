be7258be4d7b88783198f6d48f2b769f9249883b
Olga Kashapova <invispo16@gmail.com>
2023-10-09 13:12:39 +0300

    First commit

---
+++ src/components/Meal.vue
@@ -0,0 +1,17 @@
+<template>
+    <div v-if="shortcutView">
+        <header>
+            <slot name="meal"></slot>
+        </header>
+        <main>
+            <span></span>
+        </main>
+        <hr>
+        <footer>
+
+        </footer>
+    </div>
+</template>
+
+<script setup>
+</script>
